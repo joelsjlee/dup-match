fff808ebaf3284a4247e7754940e9b50cabc20e7
Joel Lee <34225086+joelslee@users.noreply.github.com>
2023-03-02 15:05:32 -0500

    update description

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
-import plotly.graph_objects as go
 dfA = pd.read_csv("full-huf.csv")
 st.title('HUF Duplicates')
-number = st.number_input('Move the index up or down to see different matches', value=0)
+st.write('Out of 54023 entries (Published, all events, newspapers with coverage) there are 1781 known articles where there exists at least two identical copies. Out of these 1781 article duplicates, there are 4313 known entries, because some articles (ex. type in 5. One article but 6 duplicate entries) there exists many duplicate copies.')
+number = st.number_input('Change the index from 0 to 1780 see different matches.', value=0)
 st.write('Index', number)
 with open("full_match.json", "r") as fp:
     matches = json.load(fp)
